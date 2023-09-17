c8b8f53ddca9f4c126fbdefe9c9a5a4ba8ad2144
magn3144 <magnus@elgaard.com>
2023-09-17 11:00:04 +0200

    Trying to make save state for popup but failed

--- Browser Exstension/popup.js
+++ Browser Exstension/popup.js
@@ -14,28 +14,40 @@ function sendStopMessage() {
 
 function initializeStartButton() {
   console.log("popup.js: initializing start button");
-  window.selectionMode = false;
-  console.log("popup.js: DOMContentLoaded");
+  let selectionMode = localStorage.getItem('selectionMode');
+  console.log("popup.js: selection mode 1: " + selectionMode);
+  if (selectionMode === null) {
+    localStorage.setItem('selectionMode', false);
+    selectionMode = false;
+  }
+  else {
+    selectionMode = (selectionMode === 'true');  // Convert string to boolean
+  }
+  console.log("popup.js: selection mode 2: " + selectionMode);
   let startButton = document.getElementById('start');
   startButton.addEventListener('click', function() {
-    console.log("popup.js: start button clicked");
-    if (window.selectionMode) {
+    console.log("popup.js: selection mode 3: " + selectionMode);
+    if (selectionMode) {
       // If already in selection mode, and the button is clicked, then stop selection mode
       startButton.textContent = "Start Selecting";
       sendStopMessage();
-      window.selectionMode = false;
+      selectionMode = false;
     }
     else {
       // If not in selection mode, and the button is clicked, then start selection mode
       startButton.textContent = "Stop Selecting";
       sendStartMessage();
-      window.selectionMode = true;
+      selectionMode = true;
     }
+    console.log("popup.js: selection mode 4: " + selectionMode);
+    localStorage.setItem('selectionMode', selectionMode);
   });
 }
 
 function initializeMessageListener() {
   console.log("popup.js: initializing message listener");
+  const port = chrome.runtime.connect({ name: "popup" });
+  port.postMessage("getLatest");
   port.onMessage.addListener(function(msg) {
     if (msg !== null) {
       console.log("popup.js: received message: " + msg);
@@ -45,47 +57,5 @@ function initializeMessageListener() {
   });
 }
 
-function checkFlagAndRun() {
-  function checkFlag() {
-    window.myExtensionHasRun = window.myExtensionHasRun || false;
-    return window.myExtensionHasRun;
-  }
-  
-  async function getActiveTabID() {
-    let queryOptions = { active: true, currentWindow: true };
-    let tabs = await chrome.tabs.query(queryOptions);
-    return tabs[0].id;
-  }
-
-  // Inject code to check for flag
-  let activeTabID = 0;
-  getActiveTabID().then((tabIdResult) => {
-    activeTabID = tabIdResult;
-  }).then(() => {
-    chrome.scripting.executeScript({ target: {tabId: activeTabID}, func: checkFlag }, (hasRunResult) => {
-      if (chrome.runtime.lastError) {
-        console.error(chrome.runtime.lastError);
-        return;
-      }
-
-      const hasRun = hasRunResult[0].result;
-      if (!hasRun) {
-        console.log("This is the first time the extension has run on this page");
-
-        function setFlagTrue() {
-          window.myExtensionHasRun = true;
-        }
-  
-        // Inject code to set flag
-        chrome.scripting.executeScript({ target: {tabId: activeTabID}, func: setFlagTrue });
-  
-        initializeStartButton();
-        initializeMessageListener();
-      }
-    });
-  });
-}
-
-const port = chrome.runtime.connect({ name: "popup" });
-//port.postMessage("getLatest");
-checkFlagAndRun();
+initializeStartButton();
+initializeMessageListener();
