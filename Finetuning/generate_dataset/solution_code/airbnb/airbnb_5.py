1535953a86f6cf40b0d3c4729e518f50ea053c3e
magn3144 <magnus@elgaard.com>
2023-11-23 17:04:35 +0100

    Fixed some airbnb scripts

--- Finetuning/generate_dataset/solution_code/airbnb/airbnb_5.py
+++ Finetuning/generate_dataset/solution_code/airbnb/airbnb_5.py
@@ -1,16 +1,28 @@
 import csv
-import lxml.html as lh
+from bs4 import BeautifulSoup
 
+# Load the HTML data from local file.
 with open('downloaded_pages/airbnb.html', 'r') as file:
-    page_content = file.read()
+    page_content = file.read().replace('\n', '')
 
-tree = lh.fromstring(page_content)
+soup = BeautifulSoup(page_content, 'html.parser')
+divs = soup.find_all("div", {"class": "t1jojoys dir dir-ltr"})
+divs_text = [div.text for div in divs]
 
-locations = tree.xpath('//div[contains(@class, "t1jojoys dir dir-ltr")]/text()')
-links = tree.xpath('//a[contains(@class, "l1ovpqvx c1kblhex dir dir-ltr")]/@href')
+# Link elements for each location
+# The parent element
+location_divs = soup.find_all("div", {"class": "dir dir-ltr"})
+# The first <a> child element
+location_link_elems = [div.find('a') for div in location_divs][1:]
+# The link for each location (checks if the link exists)
+location_links = [link['href'] if link else None for link in location_link_elems]
+# Remove empty strings from the list
+location_links = list(filter(None, location_links))
+# Remove dublicates
+location_links = list(dict.fromkeys(location_links))
 
-data = zip(locations, links)
-
-with open('scraped_data.csv', 'w') as file:
+# Save the data to a CSV file in two seperate columns.
+with open('scraped_data.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerows(data)
+    for div, span in zip(divs_text, location_links):
+        writer.writerow([div, span])
--- Finetuning/generate_dataset/solution_code/airbnb/airbnb_6.py
+++ Finetuning/generate_dataset/solution_code/airbnb/airbnb_6.py
@@ -7,23 +7,27 @@ def extract_data(file):
         contents = f.read()
 
     soup = BeautifulSoup(contents, 'lxml')
-    section = soup.find_all('h2', {'class': 'hifxi0b dir dir-ltr'})
-    for sec in section:
-        if sec.text == 'Inspiration for future getaways':
-            target_div = sec.find_parent('div')
-            locations = target_div.find_all('div', {'class': 't1jojoys dir dir-ltr'})
-            records = []
-            for location in locations:
-                text = location.text
-                link = location.find_previous_sibling('a')['href']
-                records.append((text, link))
+    
+    # Get the <ul> parent element
+    ul = soup.find('ul', {'class': 'gjo09wt dir dir-ltr'})
 
-    with open('scraped_data.csv', 'w', newline='', encoding='utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerow(['Location Name', 'Location Link'])
-        writer.writerows(records)
+    # Get the <li> child elements with class "dir dir-ltr"
+    li_elems = ul.find_all('li', {'class': 'dir dir-ltr'})
 
+    # Get the link from the <a> child elements
+    links_elems = [li.find('a') for li in li_elems]
+    links = [link['href'] if link else None for link in links_elems]
 
-if __name__ == "__main__":
-    file_path = os.path.join('downloaded_pages', 'airbnb.html')
-    extract_data(file_path)
+    # Get the text from the first <span> child element
+    spans_elems = [li.find('span') for li in li_elems]
+    spans = [span.text if span else None for span in spans_elems]
+
+    # Save the two lists as seperate columns in a CSV file
+    with open('scraped_data.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Location", "Link"])
+        for span, link in zip(spans, links):
+            writer.writerow([span, link])
+
+file_path = os.path.join('downloaded_pages', 'airbnb.html')
+extract_data(file_path)
