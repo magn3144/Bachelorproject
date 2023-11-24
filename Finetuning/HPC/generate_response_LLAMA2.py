ba527bcfdd52033c813f28efe5f84d96add27e4b
magn3144 <magnus@elgaard.com>
2023-11-24 14:38:18 +0100

    generate responses llama2

---
+++ Finetuning/HPC/generate_response_LLAMA2.py
@@ -0,0 +1,60 @@
+from transformers import AutoModelForCausalLM,AutoTokenizer
+from peft import PeftModel
+import os
+import torch
+from datasets import load_dataset
+import json
+
+
+def load_finetuned_model(base_model_name, model_directory, finetune_name):
+    base_model = AutoModelForCausalLM.from_pretrained(
+        base_model_name,
+        low_cpu_mem_usage=True,
+        return_dict=True,
+        torch_dtype=torch.float16,
+        device_map= {"": 0})
+
+    print(model_directory + "/" + finetune_name)
+    model = PeftModel.from_pretrained(base_model, model_directory + "/" + finetune_name)
+    model = model.merge_and_unload()
+
+    # Reload tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(base_model_name, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"
+
+    return model, tokenizer
+
+def generate_response(model, tokenizer, prompt):
+    runtimeFlag = "cuda:0"
+    batch = tokenizer(prompt, return_tensors='pt', return_token_type_ids=False).to(runtimeFlag)
+    with torch.cuda.amp.autocast():
+        output_tokens = model.generate(**batch, max_new_tokens=2000)
+    return tokenizer.decode(output_tokens[0], skip_special_tokens=True)
+
+def get_prompt(dataset, index):
+    return dataset[index] + " ### Response: ```"
+
+
+folder_name = "final_model"
+dataset_name = "magnus42/GPTWebScrapingPythonCode"
+base_model_name = "codellama/CodeLlama-7b-Python-hf"
+save_directory = f"trained_models/{folder_name}"
+test_set = load_dataset(dataset_name, split="test")["prompt"]
+response_dict = {}
+
+# For each folder in the save directory
+for finetune_name in os.listdir(save_directory):
+    if ("finetuned" not in finetune_name):
+        continue
+    model, tokenizer = load_finetuned_model(base_model_name, save_directory, finetune_name)
+    for i in range(len(test_set)):
+        prompt = get_prompt(test_set, i)
+        response = generate_response(model, tokenizer, prompt)
+        if not response_dict[finetune_name]:
+            response_dict[finetune_name] = []
+        response_dict[finetune_name].append(response)
+
+# Save the respose_dict as a json file using json.dump
+with open(f"trained_models/{folder_name}/generated_responses.json", "w") as file:
+    json.dump(response_dict, file)
